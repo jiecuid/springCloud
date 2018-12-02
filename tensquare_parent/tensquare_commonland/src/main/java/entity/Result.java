469a26ac8e21f5c1071f07101e266b5482fa17c8
jiecuid <15389664340@163.com>
2018-12-03 00:39:12 +0800

    通用模块的搭建

---
+++ tensquare_parent/tensquare_commonland/src/main/java/entity/Result.java
@@ -0,0 +1,68 @@
+package entity;
+
+/**
+ * Created by CuiJie on 2018/12/1.
+ * 返回结果实体类
+ */
+
+public class Result {
+
+    //是否成功
+    private boolean flag;
+    //返回码
+    private Integer code;
+    //返回信息
+    private String message;
+    //返回数据
+    private Object data;
+
+    public Result() {
+    }
+
+    //适用于增删改
+    public Result(boolean flag, Integer code, String message) {
+        this.flag = flag;
+        this.code = code;
+        this.message = message;
+    }
+
+    //适用于查询
+    public Result(boolean flag, Integer code, String message, Object data) {
+        this.flag = flag;
+        this.code = code;
+        this.message = message;
+        this.data = data;
+    }
+
+    public boolean isFlag() {
+        return flag;
+    }
+
+    public void setFlag(boolean flag) {
+        this.flag = flag;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+}
